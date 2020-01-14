4fe9cf889fc2c57bf98436f294d7a8a9d0fe6646
Micko Matamorosa <micko.matamorosa@boom.camp>
2020-01-14 10:34:05 +0800

    first commit

---
+++ src/components/Postform.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Postform extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default Postform
